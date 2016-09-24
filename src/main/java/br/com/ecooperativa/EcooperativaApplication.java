40075f465f6b3d4ecb0ac816c795c84591e7541d
joaohoracio38 <joaolago35@gmail.com>
2016-09-24 10:32:28 -0300

    primeiro commits

---
+++ src/main/java/br/com/ecooperativa/EcooperativaApplication.java
@@ -0,0 +1,12 @@
+package br.com.ecooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcooperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcooperativaApplication.class, args);
+	}
+}
